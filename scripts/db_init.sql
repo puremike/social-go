1e7bd7dea0bc7c370683573ae39b35bcf6c8d77f
puremike <michaelmarketing25@gmail.com>
2024-11-14 22:05:49 +0100

    made migration to db

--- scripts/db_init.sql
+++ scripts/db_init.sql
@@ -1,1 +1,7 @@
--- CREATE DATABASE
+CREATE DATABASE social2
+CREATE EXTENSION IF NOT EXISTS citext;
+
+
+-- Create migration: migrate create -ext sql -dir cmd/migrate/migrations -seq create_users_table
+
+-- Make migration: migrate -database postgres://user:password@localhost/db_name?sslmode=disable -path ./cmd/api/migrate/migrations up
